a6be3b6156ff4c02f40ba022f2bd0f3917b0d5ad
dirty-nine <nrwgalaxy@gmail.com>
2026-01-07 20:16:15 -0800

    finished sesh 2 work and made numpyTest.py

--- session2.py
+++ session2.py
@@ -2,3 +2,16 @@
 Write a Python program that prints the sum of two floating point numbers, the difference between two integers, and the product of a floating point number and an integer. In each case, have the program print out the data type of the resulting answer.
 '''
 
+print(f'''
+Sum of two floating point ints: 
+3.2 + 0.11 = {3.2 + 0.11} 
+(ans type:{type(3.2 + 0.11)})
+
+Difference between two ints:
+5 + 1 = {5+1} 
+(ans type:{type(5+1)})
+
+Product of a float and an int:
+8.999 * 9 = {8.999 * 1} 
+(ans type:{type(8.999 * 1)})
+''')
