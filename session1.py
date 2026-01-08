6164c2a471f07c9baea7a3bbe99be6f893122147
dirty-nine <nrwgalaxy@gmail.com>
2026-01-07 20:04:00 -0800

    first commit

---
+++ session1.py
@@ -0,0 +1,6 @@
+print('''
+Hi! My name is Nathaniel Wan
+My preferred pronouns are he/him
+My favorite movie is Konosuba Ledgend of Crimson. I love the intricacies in the character interactions and how funny it is.
+My favorite food is chicken. I love that chicken is so versitile. Chicken sandwiches, chicken tenders, chicken nuggets, chicken broth, chicken noodle soup, chicken and waffles, chicken on pizza, chichen flavored ramen -- chicken tastes so good no matter what it is used for!
+      ''')
