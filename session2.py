8554ad6a503caf0f001206decf21f3745352d7e9
dirty-nine <nrwgalaxy@gmail.com>
2026-01-07 20:08:27 -0800

    commit 1

---
+++ session2.py
@@ -0,0 +1,4 @@
+'''
+Write a Python program that prints the sum of two floating point numbers, the difference between two integers, and the product of a floating point number and an integer. In each case, have the program print out the data type of the resulting answer.
+'''
+
