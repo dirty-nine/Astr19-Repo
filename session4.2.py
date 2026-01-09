93f63955775c0397ac52deb47871450472f2a732
dirty-nine <nrwgalaxy@gmail.com>
2026-01-08 17:37:50 -0800

    added ver 2 of session4.py

---
+++ session4.2.py
@@ -0,0 +1,16 @@
+'''
+Write a Python program that declares a class describing your favorite animal. Have the data members of the class represent the following physical parameters of the animal: length of the arms (float), length of the legs (float), number of eyes (int), does it have a tail? (bool), is it furry? (bool). Write an initialization function that sets the values of the data members when an instance of the class is created. Write a member function of the class to print out and describe the data members representing the physical characteristics of the animal
+'''
+class Animal:
+    def __init__(self):
+        self.arm_len = 20.5
+        self.leg_len = 20.1
+        self.num_eyes = 2
+        self.has_tail = True
+        self.is_furry = False
+
+    def __str__(self):
+        return(f"My favorite animal has an arm and leg length of {self.arm_len}cm and {self.leg_len}cm respectively. It has {self.num_eyes} eye{"s" if self.num_eyes > 1 else ""}, has {"a" if self.has_tail else "no"} tail, and is{"n't" if not self.is_furry else ""} furry.")
+
+dog = Animal()
+print(dog)
