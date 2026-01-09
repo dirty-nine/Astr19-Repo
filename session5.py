d4cb1b29cba8de639e0124158401bfe34f2d08dc
dirty-nine <nrwgalaxy@gmail.com>
2026-01-08 17:40:04 -0800

    initialized session5.py

---
+++ session5.py
@@ -0,0 +1,4 @@
+'''Write a Python program that writes out a table of the function sin(x) vs. x, where x is tabulated between 0 and 2 with a thousand entries. Follow the basic Python program structure, including a main program function'''
+
+def main():
+    pass
