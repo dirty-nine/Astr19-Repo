a9220bca560e2ffebc40e775b8a325ab550ca3d4
dirty-nine <nrwgalaxy@gmail.com>
2026-01-08 17:41:25 -0800

    initialized session6.py

---
+++ session6.py
@@ -0,0 +1,6 @@
+'''
+Create a Jupyter Notebook where, in separate cells, you define functions that return sin(x) and cos(x).  Use Markdown cells to comment your Notebook, and describe what each function does. Create a third Python cell that will tabulate sin(x) and cos(x) using these previously defined functions vs. x, where x is tabulated between 0 and 2 with a thousand entries. Write a fourth Python cell that will use a for loop to print out the first 10 values of x, sin(x), and cos(x) in columns
+'''
+
+def main():
+    pass
