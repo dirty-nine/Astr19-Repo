23640998aef0d2dd3adf4d6c2bd1575859d41cb8
dirty-nine <nrwgalaxy@gmail.com>
2026-01-23 16:51:46 -0800

    final sesh5.py commit

--- session5.py
+++ session5.py
@@ -1,10 +1,10 @@
-'''Write a Python program that writes out a table of the function sin(x) vs. x, where x is tabulated between 0 and 2 with a thousand entries. Follow the basic Python program structure, including a main program function'''
+'''Write a Python program that writes out a table of the function sin(x) vs. x, where x is tabulated between 0 and 2pi with a thousand entries. Follow the basic Python program structure, including a main program function'''
 
 import numpy as np
 from astropy.table import Table
 
 def main():
-    x = np.linspace(0, 2, 1000)
+    x = np.linspace(0, np.pi, 1000)
     sinx = np.sin(x)    
     #print(np.transpose((sinx, x)))
 
